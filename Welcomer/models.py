68e5cea88c2446d05f5d08a853d623a277e71014
31124ya <Minginbiec@gmail.com>
2026-09-18 22:33:37 +0800

    Updated

--- Welcomer/models.py
+++ Welcomer/models.py
@@ -1,22 +1,14 @@
 import string
 
 
-class SafeFormat(string.Formatter):
-    def get_field(self, field_name, args, kwargs):
-        try:
-            return super().get_field(field_name, args, kwargs)
-        except (KeyError, AttributeError, IndexError):
-            return "", field_name
-
-
 class SafeString(str):
     def __getattr__(self, item):
         return ""
 
 
-def apply_vars(message, member=None, guild=None, bot=None, invite=None):
+def apply_vars(bot, member, message, invite=None):
     """
-    Replace variables in the welcome message.
+    Replace Welcomer variables.
 
     Supported:
         {member}
@@ -29,42 +21,38 @@ def apply_vars(message, member=None, guild=None, bot=None, invite=None):
         {guild.id}
 
         {bot}
-        {bot.name}
-
         {invite}
     """
 
-    class Obj:
-        def __init__(self, obj):
-            self._obj = obj
+    if not isinstance(message, str):
+        return message
 
-        def __getattr__(self, name):
-            if self._obj is None:
-                return SafeString("")
-
-            try:
-                value = getattr(self._obj, name)
-                return SafeString(str(value))
-            except (AttributeError, TypeError):
-                return SafeString("")
-
-        def __str__(self):
-            if self._obj is None:
-                return ""
-
-            try:
-                return str(self._obj)
-            except Exception:
-                return ""
+    guild = getattr(member, "guild", None)
 
     variables = {
-        "member": Obj(member),
-        "guild": Obj(guild),
-        "bot": Obj(bot),
-        "invite": SafeString(invite or ""),
+        "member": member,
+        "guild": guild,
+        "bot": bot,
+        "invite": invite or "",
     }
 
+    class SafeFormatter(string.Formatter):
+
+        def get_field(self, field_name, args, kwargs):
+            try:
+                return super().get_field(
+                    field_name,
+                    args,
+                    kwargs
+                )
+            except (KeyError, AttributeError, IndexError):
+                return "", field_name
+
     try:
-        return SafeFormat().format(message, **variables)
+        return SafeFormatter().format(
+            message,
+            **variables
+        )
+
     except Exception:
         return message
